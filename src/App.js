6ae72aeaea6c317131226a8e0829a19ef9abb6a1
Ritu <ritutwinkle10@gmail.com>
2024-02-01 00:36:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
         <p>
           Deployment of Full stack app to AWS Cloud.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
     </div>
   );
 }
